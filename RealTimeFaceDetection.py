79755bb5f4ea1643544e5ab6afda6294b8172a73
AvivaHebbare23 <eviltoastofdoom@gmail.com>
2025-07-24 12:48:06 +0530

    AI Lesson 12

--- RealTimeFaceDetection.py
+++ RealTimeFaceDetection.py
@@ -1,5 +1,5 @@
 import cv2
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalcatface_extended.xml')
 
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
